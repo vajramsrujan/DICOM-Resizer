df21a5983c34956c90f65d6b778b2f301dd885e2
vajramsrujan <55774184+vajramsrujan@users.noreply.github.com>
2020-10-15 00:37:09 -0400

    Update dicom_resize.py

--- dicom_resize.py
+++ dicom_resize.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 21 17:18:41 2020
-
-@author: srujanvajram
-"""
 # =========================================================================== #
 # Import relevant packages 
 
